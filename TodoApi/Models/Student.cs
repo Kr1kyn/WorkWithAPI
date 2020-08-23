da4b74948012654ce7c2f6e5c7b702a39c4a12af
Konstantin_st <Kostya_ogir@mail.ru>
2020-08-23 22:20:48 +0300

    Add Course Lection, Lecturer and Student classes

---
+++ TodoApi/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
